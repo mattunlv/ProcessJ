f1807bc90e43f13dd00354dd96cef3323d552204
Carlos Cuenca <cuenca.cl@gmail.com>
2022-03-12 22:39:28 -0800

    Add ProcessJRuntime.hpp

---
+++ processj/src/cppruntime/include/ProcessJRuntime.hpp
@@ -0,0 +1,25 @@
+#ifndef UNLV_PROCESS_J_RUNTIME_HPP
+#define UNLV_PROCESS_J_RUNTIME_HPP
+
+/* include guards for the runtime types */
+//#include <pj_logger.hpp>
+//#include <pj_process.hpp>
+//#include <pj_timer.hpp>
+//#include <pj_timer_queue.hpp>
+//#include <pj_run_queue.hpp>
+//#include <pj_inactive_pool.hpp>
+//#include <pj_scheduler.hpp>
+//#include <pj_channel.hpp>
+//#include <pj_one2one_channel.hpp>
+//#include <pj_one2many_channel.hpp>
+//#include <pj_many2one_channel.hpp>
+//#include <pj_many2many_channel.hpp>
+//#include <pj_barrier.hpp>
+//#include <pj_alt.hpp>
+//#include <pj_record.hpp>
+//#include <pj_protocol.hpp>
+//#include <pj_par.hpp>
+//#include <pj_array.hpp>
+//#include <pj_string.hpp>
+
+#endif
