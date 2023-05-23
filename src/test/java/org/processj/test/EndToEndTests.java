743a824374a91e18cd4d8dedf535fd8cb8564e46
Carlos <Cuenca>
2023-05-22 18:29:20 -0700

    Add Book Code Chapter 3 Section 5 End-to-End test

--- src/test/java/org/processj/test/EndToEndTests.java
+++ src/test/java/org/processj/test/EndToEndTests.java
@@ -3,15 +3,26 @@ package org.processj.test;
 import org.junit.jupiter.api.Test;
 import org.processj.ProcessJc;
 
-public class EndToEndTests {
+public class EndToEndTests extends ProcessJTest {
 
     @Test
     public void bookCodeChapter1Section2_endToEnd() {
 
-        final String[] arguments = {"src/test/resources/code/book/chapter_1/section_2/synchronized_communication.pj"};
+        final String[] arguments = {CODE_BOOK_1_2_SYNCHRONIZED_COMMUNICATION};
 
         ProcessJc.main(arguments);
 
     }
+
+    @Test
+    public void bookCodeChapter3Section5_endToEnd() {
+
+        final String[] arguments = {CODE_BOOK_3_5_TIME_IN_PROCESS_J};
+
+        ProcessJc.main(arguments);
+
+    }
+
+
 
 }
--- src/test/java/org/processj/test/ProcessJTest.java
+++ src/test/java/org/processj/test/ProcessJTest.java
@@ -5,8 +5,50 @@ public class ProcessJTest {
     /// --------------------------
     /// Protected Static Constants
 
-    protected final static String CODE_BOOK_1_2_SYNCHRONIZED_COMMUNICATION = "src/test/resources/code/book/chapter_1/section_2/synchronized_communication.pj";
-    protected final static String CODE_BOOK_3_5_TIME_IN_PROCESS_J          = "src/test/resources/code/book/chapter_3/section_5/time_in_process_j.pj";
-
-
+    protected final static String CODE_BOOK_1_2_SYNCHRONIZED_COMMUNICATION
+            = "src/test/resources/code/book/chapter_1/section_2/synchronized_communication.pj";
+    protected final static String CODE_BOOK_3_5_TIME_IN_PROCESS_J
+            = "src/test/resources/code/book/chapter_3/section_5/time_in_process_j.pj";
+    protected final static String CODE_BOOK_3_6_ONE_TO_ONE_CHANNELS
+            = "src/test/resources/code/book/chapter_3/section_6/one_to_one_channels.pj";
+    protected final static String CODE_BOOK_3_7_FIBONACCI
+            = "src/test/resources/code/book/chapter_3/section_7/fibonacci.pj";
+    protected final static String CODE_BOOK_3_7_FIFO6
+            = "src/test/resources/code/book/chapter_3/section_7/fifo6.pj";
+    protected final static String CODE_BOOK_3_7_INTEGRATE
+            = "src/test/resources/code/book/chapter_3/section_7/integrate.pj";
+    protected final static String CODE_BOOK_3_7_NUMBERS
+            = "src/test/resources/code/book/chapter_3/section_7/numbers.pj";
+    protected final static String CODE_BOOK_3_7_PAIRS
+            = "src/test/resources/code/book/chapter_3/section_7/pairs.pj";
+    protected final static String CODE_BOOK_3_7_SQUARES
+            = "src/test/resources/code/book/chapter_3/section_7/squares.pj";
+    protected final static String CODE_BOOK_3_7_STRUCTURED_EXECUTION
+            = "src/test/resources/code/book/chapter_3/section_7/structured_execution.pj";
+    protected final static String CODE_BOOK_3_8_EIGHT_BIT_ADDER
+            = "src/test/resources/code/book/chapter_3/section_8/eight_bit_adder.pj";
+    protected final static String CODE_BOOK_3_8_FOUR_BIT_ADDER
+            = "src/test/resources/code/book/chapter_3/section_8/four_bit_adder.pj";
+    protected final static String CODE_BOOK_3_7_ONE_BIT_ADDER
+            = "src/test/resources/code/book/chapter_3/section_8/one_bit_adder.pj";
+    protected final static String CODE_BOOK_3_9_OUTPUT
+            = "src/test/resources/code/book/chapter_3/section_9/output.pj";
+    protected final static String CODE_BOOK_4_1_SWITCH
+            = "src/test/resources/code/book/chapter_4/section_1/switch.pj";
+    protected final static String CODE_BOOK_4_2_FOR
+            = "src/test/resources/code/book/chapter_4/section_2/for.pj";
+    protected final static String CODE_BOOK_4_2_WHILE
+            = "src/test/resources/code/book/chapter_4/section_2/while.pj";
+    protected final static String CODE_BOOK_4_3_REPETITION
+            = "src/test/resources/code/book/chapter_4/section_3/repetition.pj";
+    protected final static String CODE_BOOK_4_4_ALT
+            = "src/test/resources/code/book/chapter_4/section_4/alt.pj";
+    protected final static String CODE_BOOK_4_4_MUX
+            = "src/test/resources/code/book/chapter_4/section_4/mux.pj";
+    protected final static String CODE_BOOK_4_4_TIMEOUT_GUARD
+            = "src/test/resources/code/book/chapter_4/section_4/timeoutguard.pj";
+    protected final static String CODE_BOOK_4_4_WATCHDOG
+            = "src/test/resources/code/book/chapter_4/section_4/watchdog.pj";
+    protected final static String CODE_BOOK_4_5_PRI_ALT
+            = "src/test/resources/code/book/chapter_4/section_5/pri_alt.pj";
 }
