6e29b3e5716438e7495bb73b0c6541bca26b26e7
Carlos <Cuenca>
2023-05-22 18:32:45 -0700

    Add Book Code Chapter 3 Section 7 End-to-End test

--- src/test/java/org/processj/test/EndToEndTests.java
+++ src/test/java/org/processj/test/EndToEndTests.java
@@ -6,7 +6,7 @@ import org.processj.ProcessJc;
 public class EndToEndTests extends ProcessJTest {
 
     @Test
-    public void bookCodeChapter1Section2_endToEnd() {
+    public void bookCodeChapter1Section2_synchronizedCommunication_endToEnd() {
 
         final String[] arguments = {CODE_BOOK_1_2_SYNCHRONIZED_COMMUNICATION};
 
@@ -15,7 +15,7 @@ public class EndToEndTests extends ProcessJTest {
     }
 
     @Test
-    public void bookCodeChapter3Section5_endToEnd() {
+    public void bookCodeChapter3Section5_timeInProcessJ_endToEnd() {
 
         final String[] arguments = {CODE_BOOK_3_5_TIME_IN_PROCESS_J};
 
@@ -23,6 +23,22 @@ public class EndToEndTests extends ProcessJTest {
 
     }
 
+    @Test
+    public void bookCodeChapter3Section6_endToEnd() {
 
+        final String[] arguments = {CODE_BOOK_3_6_ONE_TO_ONE_CHANNELS};
+
+        ProcessJc.main(arguments);
+
+    }
+
+    @Test
+    public void bookCodeChapter3Section7_fibonacci_endToEnd() {
+
+        final String[] arguments = {CODE_BOOK_3_7_FIBONACCI};
+
+        ProcessJc.main(arguments);
+
+    }
 
 }
