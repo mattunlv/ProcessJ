1af189a1b6219520494fd16d3d6755e61f7d779e
Carlos <Cuenca>
2023-06-01 14:33:24 -0700

    Add Files#IsFile

--- src/main/java/org/processj/utilities/Files.java
+++ src/main/java/org/processj/utilities/Files.java
@@ -31,4 +31,31 @@ public class Files {
 
     }
 
+    /**
+     * <p>Returns a flag indicating if the specified {@link String} path exists & is a file.</p>
+     * @param path {@link String} value representing a path.
+     * @return Flag indicating if the specified {@link String} path exists & is a file.
+     * @since 0.1.0
+     */
+    public static boolean IsFile(final String path) {
+
+        // Initialize the result
+        boolean result = path != null;
+
+        // If the path is valid
+        if(result) {
+
+            // Instantiate a file
+            final File file = new File(path);
+
+            // Set the final result
+            result = file.exists() && file.isFile();
+
+        }
+
+        // Return the result
+        return result;
+
+    }
+
 }
